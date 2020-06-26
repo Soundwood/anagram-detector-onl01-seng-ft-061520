8ab015dbc8e50258dbd8abc2ccb618f25f6877d6
Ben Hogan <j.benhogan@gmail.com>
2020-06-26 22:37:21 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,14 +2,15 @@ require'pry'
 
 # Your code goes here!
 class Anagram
-  attr_accessor 
+  attr_accessor :word_sorted
   
   def initialize(word)
     @word = word
+    @word_sorted = word.split.sort 
   end
   def match(words_array)
-    words_array.each do |word|
-      word.split("")
-  end
+    words_array.map do |word|
+      word.split("").sort == @word_sorted
+    end
   end
 end
